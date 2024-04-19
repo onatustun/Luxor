5e657f4d83cb78b09521de9dd2e0874bf8e726cc
Onat Ustun <onatustun@gmail.com>
2024-04-19 21:29:56 +0100

    UPDATE: session storage
    UPDATE: luxor logo stays

--- resources/js/userPortal.js
+++ resources/js/userPortal.js
@@ -9,23 +9,42 @@ $(document).ready(function () {
         signUp: 'Sign Up'
     }
 
+    function displayUserForm() {
+        $('#page-loader').css('display','flex')
+
+        setTimeout(function () {
+            $('#user-form').fadeIn(300).css('display', 'flex')
+        }, 1500)
+
+        setTimeout(function () {
+            $('#luxor-logo').fadeIn(105)
+        }, 500)
+
+        setTimeout(function () {
+            $('#luxor-logo').css({
+                'height': '3.5rem',
+                'top': '5%',
+                'fill': 'var(--bright-blue)'
+            })
+        }, 1500)
+    }
+
+    const sessionVisit = sessionStorage.getItem('visited');
+
+    if (!sessionVisit) {
+        displayUserForm()
+        sessionStorage.setItem('visited', 1)
+    } else {
+        $('#luxor-logo').css({
+            'display': 'block',
+            'height': '3.5rem',
+            'top': '5%',
+            'fill': 'var(--bright-blue)',
+            'animation':'none'
+        })
+    }
+
     $('#account-checker').html(`${accountCheckerOptions.alreadyHaveAccount}?`)
     $('#user-redirect').html(userRedirectOptions.login)
     $('#user-submit').html(userRedirectOptions.signUp)
-
-    setTimeout(function () {
-        $('#user-form').fadeIn(300).css('display', 'flex')
-    }, 1500)
-
-    setTimeout(function () {
-        $('#luxor-logo').fadeIn(105)
-    }, 500)
-
-    setTimeout(function () {
-        $('#luxor-logo').css({
-            'height': '3.5rem',
-            'top': '5%',
-            'fill': 'var(--bright-blue)'
-        })
-    }, 1500)
 })
